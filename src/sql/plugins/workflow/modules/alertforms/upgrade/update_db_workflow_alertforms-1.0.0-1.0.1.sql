3e07cd8f8b49785732e68ff60c1902ca9b589761
obfromevi <obfromevi@gmail.com>
2025-12-02 11:24:43 +0100

    LUT-31355 : add liquibase tags to SQL files

--- src/sql/plugins/workflow/modules/alertforms/upgrade/update_db_workflow_alertforms-1.0.0-1.0.1.sql
+++ src/sql/plugins/workflow/modules/alertforms/upgrade/update_db_workflow_alertforms-1.0.0-1.0.1.sql
@@ -1,1 +1,4 @@
+-- liquibase formatted sql
+-- changeset workflow-alertforms:update_db_workflow_alertforms-1.0.0-1.0.1.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 ALTER TABLE task_alert ADD is_executed SMALLINT DEFAULT 1 NOT NULL;
